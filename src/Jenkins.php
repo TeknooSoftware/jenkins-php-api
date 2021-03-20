4dc64583f76ba56d03f642f37efd441b0e9f01ee
Matthew Wendel <matt_wendel@hotmail.com>
2021-03-19 21:35:10 -0400

    fix improper type declaration in docblock also repair incorrect ref to global Exception class from within a namespace.

--- src/Jenkins.php
+++ src/Jenkins.php
@@ -11,7 +11,7 @@ class Jenkins
     private $baseUrl;
 
     /**
-     * @var null
+     * @var stdClass
      */
     private $jenkins = null;
 
@@ -132,7 +132,7 @@ class Jenkins
         } else {
             try {
                 $this->getQueue();
-            } catch (RuntimeException $e) {
+            } catch (\RuntimeException $e) {
                 //en cours de lancement de jenkins, on devrait passer par là
                 return false;
             }
